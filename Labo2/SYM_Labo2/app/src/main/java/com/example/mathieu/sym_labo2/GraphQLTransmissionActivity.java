25e09361c002d76e98733ffbc9e895b4bf9666a0
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-11-13 16:35:03 +0100

    graphql finished

--- Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/GraphQLTransmissionActivity.java
+++ Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/GraphQLTransmissionActivity.java
@@ -1,13 +1,14 @@
 package com.example.mathieu.sym_labo2;
 
-import android.os.AsyncTask;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,9 +19,10 @@ import java.util.List;
 
 public class GraphQLTransmissionActivity extends AppCompatActivity {
 
-    final String AUTHORS_URL = "{\"query\": \"{allAuthors{first_name last_name}}\"}";
+    final String API_URL = "http://sym.iict.ch/api/graphql";
     List<String> authors = new ArrayList<>();
-    ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapterSpinner;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +31,29 @@ public class GraphQLTransmissionActivity extends AppCompatActivity {
         new AsyncSendRequest(new CommunicationEventListener() {
             @Override
             public boolean handleServerResponse(String response) {
-                fillAuthors(response);
-                return true;
+                return fillAuthors(response);
             }
-        }).execute("http://sym.iict.ch/api/graphql", AUTHORS_URL, SymComManager.JSON);
+        }).execute(API_URL, "{\"query\": \"{allAuthors{first_name last_name}}\"}", SymComManager.JSON);
 
         setContentView(R.layout.activity_graph_qltransmission);
 
         Spinner spinner = findViewById(R.id.graphSpinner);
         authors.add("Loading...");
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, authors);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        adapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, authors);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterSpinner);
 
         spinner.setOnItemSelectedListener(
                 new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                         int authId = position + 1;
-                        Log.e("TAG", adapter.getItem(position));
-                        Log.e("TAG", String.valueOf(authId));
+                        new AsyncSendRequest(new CommunicationEventListener() {
+                            @Override
+                            public boolean handleServerResponse(String response) {
+                                return managePosts(response);
+                            }
+                        }).execute(API_URL, "{\"query\": \"{allPostByAuthor(authorId: " + authId + "){title content}}\"}", SymComManager.JSON);
                     }
 
                     @Override
@@ -58,8 +63,7 @@ public class GraphQLTransmissionActivity extends AppCompatActivity {
         );
     }
 
-    private void fillAuthors(String resp) {
-        Log.e("TAG", resp);
+    private boolean fillAuthors(String resp) {
         try {
             JSONObject json = new JSONObject(resp);
             JSONArray auth = json.getJSONObject("data").getJSONArray("allAuthors");
@@ -74,15 +78,53 @@ public class GraphQLTransmissionActivity extends AppCompatActivity {
             this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    adapter.clear();
-                    adapter.addAll(authors);
-                    adapter.notifyDataSetChanged();
+                    adapterSpinner.clear();
+                    adapterSpinner.addAll(authors);
+                    adapterSpinner.notifyDataSetChanged();
                 }
             });
 
+            return true;
 
         } catch (JSONException e) {
             e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean managePosts(String resp) {
+        try {
+            JSONObject json = new JSONObject(resp);
+            JSONArray posts = json.getJSONObject("data").getJSONArray("allPostByAuthor");
+
+            final String[] postsTab = new String[posts.length()];
+            String textViewContent = "";
+            for (int i = 0; i < posts.length(); ++i) {
+                String title = posts.getJSONObject(i).getString("title");
+                String content = posts.getJSONObject(i).getString("content");
+                String post = title + "\n\n" + content + "\n\n\n";
+                postsTab[i] = post;
+                textViewContent += post;
+            }
+
+            final String finalTextViewContent = textViewContent;
+            this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                        ScrollView scrollView = findViewById(R.id.graphScrollView);
+                        TextView textView = new TextView(context);
+                        textView.setText(finalTextViewContent);
+
+                        scrollView.removeAllViews();
+                        scrollView.addView(textView );
+                }
+            });
+
+            return true;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
