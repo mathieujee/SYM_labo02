3b11140997c4795a6e244f56fe4314e50f97c4ec
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-11-13 14:34:40 +0100

    SerializedTransmission via JSON, OK

--- Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/SerializedTransmissionActivity.java
+++ Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/SerializedTransmissionActivity.java
@@ -4,22 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import okhttp3.MediaType;
 
 public class SerializedTransmissionActivity extends AppCompatActivity {
 
     private EditText editFirstname;
     private EditText editLastname;
+    private TextView serverResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serialized_transmission);
 
-        editFirstname = (EditText)findViewById(R.id.firstnameField);
-        editLastname  = (EditText)findViewById(R.id.lastnameField);
+        editFirstname = (EditText) findViewById(R.id.firstnameField);
+        editLastname = (EditText) findViewById(R.id.lastnameField);
+        serverResponse = (TextView) findViewById(R.id.SerializedResponseFromServer);
+
     }
 
     public void sendJsonPayload(View view) {
@@ -30,28 +36,40 @@ public class SerializedTransmissionActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        SymComManager scm = new SymComManager();
-        scm.setCommunicationEventListener(
-                new CommunicationEventListener() {
-                    @Override
-                    public boolean handleServerResponse(String response) {
-                        System.out.println(response);
-                        return true;
-                    }
+
+        new AsyncSendRequest(new CommunicationEventListener() {
+            @Override
+            public boolean handleServerResponse(String response) {
+                if (response != null) {
+
+                    return displayServerResponse(response);
                 }
-        );
-        scm.sendRequest("http://sym.iict.ch/rest/json", jsonObject.toString(), SymComManager.JSON);
+                return false;
+            }
+        }).execute("http://sym.iict.ch/rest/json/", jsonObject.toString(), SymComManager.JSON);
     }
 
     public void sendXMLPayload() {
 
     }
 
+    private boolean displayServerResponse(final String response) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                serverResponse.setText(response);
+            }
+        });
+        return true;
+    }
+
     private JSONObject buildJson() throws JSONException {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.accumulate("firstName", editFirstname.getText().toString());
-        jsonObject.accumulate("lastName", editLastname.getText().toString());
-
+        if (editFirstname != null && editLastname != null) {
+            jsonObject.accumulate("firstName", editFirstname.getText().toString());
+            jsonObject.accumulate("lastName", editLastname.getText().toString());
+        }
+        jsonObject.accumulate("alo", "coucou");
         return jsonObject;
     }
 }
