7e349345b20138154fdd761d14e946a1b29092f1
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-11-13 13:24:28 +0100

    spinner working

--- Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/GraphQLTransmissionActivity.java
+++ Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/GraphQLTransmissionActivity.java
@@ -1,27 +1,35 @@
 package com.example.mathieu.sym_labo2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphQLTransmissionActivity extends AppCompatActivity {
 
-    void showToast(CharSequence msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
+    List<String> authors = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new AsyncAuthors().execute("http://sym.iict.ch/api/graphql", "{\"query\": \"{allAuthors{first_name last_name}}\"}");
         setContentView(R.layout.activity_graph_qltransmission);
-        getAuthors();
 
-        final Spinner spinner = findViewById(R.id.graphSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, authors);
+        Spinner spinner = findViewById(R.id.graphSpinner);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, authors);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
@@ -29,28 +37,59 @@ public class GraphQLTransmissionActivity extends AppCompatActivity {
                 new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        showToast("Spinner1: position=" + position + " id=" + id);
+                        Log.e("TAG", adapter.getItem(position));
                     }
 
                     @Override
                     public void onNothingSelected(AdapterView<?> parent) {
-                        showToast("Spinner1: unselected");
                     }
                 }
         );
+
     }
 
-    static String[] authors = new String[]{"Romain", "Oliver", "Jee", "Florent Nicolas Pillou"};
+    private class AsyncAuthors extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            SymComManager manager = new SymComManager();
+            manager.setCommunicationEventListener(new CommunicationEventListener() {
+                @Override
+                public boolean handleServerResponse(String response) {
+                    fillAuthors(response);
+                    return true;
+                }
+            });
 
-    private void getAuthors(){
-        new AsyncRequest().execute("http://sym.iict.ch/api/graphql", "{\"query\": \"{allAuthors{first_name last_name}}\"}");
+            manager.sendRequest(params[0], params[1], manager.JSON);
 
-        /*
-        JSONObject postData = new JSONObject();
+            return "123";
+        }
+    }
+
+    private void fillAuthors(String resp) {
+        Log.e("TAG", resp);
         try {
-            postData.put("query", "{allAuthors{first_name last_name}}");
+            JSONObject json = new JSONObject(resp);
+            JSONArray auth = json.getJSONObject("data").getJSONArray("allAuthors");
+            final String[] authors = new String[auth.length()];
+            for (int i = 0; i < auth.length(); ++i) {
+                String first = auth.getJSONObject(i).getString("first_name");
+                String last = auth.getJSONObject(i).getString("last_name");
+                String name = first + " " + last;
+                authors[i] = name;
+            }
+
+            this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter.addAll(authors);
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+
         } catch (JSONException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
