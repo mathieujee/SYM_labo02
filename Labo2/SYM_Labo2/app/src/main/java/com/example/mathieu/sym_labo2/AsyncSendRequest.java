239361cb53ad8a07a296eae172e2c929133a1743
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-11-25 20:01:25 +0100

    gestion headers

--- Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/AsyncSendRequest.java
+++ Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/AsyncSendRequest.java
@@ -1,6 +1,9 @@
 package com.example.mathieu.sym_labo2;
 
 import android.os.AsyncTask;
+import android.util.Pair;
+
+import java.util.List;
 
 import okhttp3.MediaType;
 
@@ -8,18 +11,27 @@ public class AsyncSendRequest extends AsyncTask<String, Void, String> {
 
     private CommunicationEventListener com;
     private boolean isDelayed = false;
+    private List<Pair<String, String>> headers;
 
-    public AsyncSendRequest(CommunicationEventListener com, boolean isDelayed){
+    public AsyncSendRequest(CommunicationEventListener com, boolean isDelayed, List<Pair<String, String>> headers){
         this.com = com;
         this.isDelayed = isDelayed;
+        this.headers = headers;
+    }
+
+    public AsyncSendRequest(CommunicationEventListener com, boolean isDelayed){
+        this(com, isDelayed, null);
     }
 
     public AsyncSendRequest(CommunicationEventListener com){
-        this(com, false);
+        this(com, false, null);
     }
 
     @Override
     protected String doInBackground(String... params) {
+        if(params.length < 3){
+            return null;
+        }
         SymComManager manager;
         if(isDelayed){
             manager = new SymComManagerDelayed();
@@ -29,8 +41,12 @@ public class AsyncSendRequest extends AsyncTask<String, Void, String> {
         }
 
         manager.setCommunicationEventListener(com);
-        manager.sendRequest(params[0], params[1], MediaType.parse(params[2]));
-
+        if(headers == null || headers.size() == 0) {
+            manager.sendRequestWithoutHeaders(params[0], params[1], MediaType.parse(params[2]));
+        }
+        else{
+            manager.sendRequestWithHeaders(params[0], params[1], MediaType.parse(params[2]), headers);
+        }
         return "";
     }
 }
--- Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/SymComManager.java
+++ Labo2/SYM_Labo2/app/src/main/java/com/example/mathieu/sym_labo2/SymComManager.java
@@ -1,7 +1,11 @@
 package com.example.mathieu.sym_labo2;
 
-import java.io.IOException;
+import android.util.Pair;
 
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,12 +19,7 @@ public class SymComManager {
     private final static OkHttpClient client = new OkHttpClient();
     private CommunicationEventListener l;
 
-    public void sendRequest(String url, String payload, MediaType type) {
-        RequestBody body = RequestBody.create(type, payload);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
+    public void sendRequest(Request request) {
         Response response;
         try {
             response = client.newCall(request).execute();
@@ -31,6 +30,29 @@ public class SymComManager {
             e.printStackTrace();
             l.handleServerResponse("ERROR, the server did not answer to the request");
         }
+    }
+
+    public void sendRequestWithoutHeaders(String url, String payload, MediaType type) {
+        RequestBody body = RequestBody.create(type, payload);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        sendRequest(request);
+    }
+
+    public void sendRequestWithHeaders(String url, String payload, MediaType type, List<Pair<String, String>> headers) {
+        RequestBody body = RequestBody.create(type, payload);
+        Headers.Builder h = new Headers.Builder();
+        for (int i = 0; i < headers.size(); i++){
+            h.add(headers.get(i).first, headers.get(i).second);
+        }
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .headers(h.build())
+                .build();
+        sendRequest(request);
     }
 
     public void setCommunicationEventListener (CommunicationEventListener l){
