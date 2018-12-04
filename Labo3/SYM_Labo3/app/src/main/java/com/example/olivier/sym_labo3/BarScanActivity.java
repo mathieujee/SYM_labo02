0fe5251500c258478e6afb6e07d4ed2685f4e437
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-12-04 09:10:54 +0100

    barscan

--- Labo3/SYM_Labo3/app/src/main/java/com/example/olivier/sym_labo3/BarScanActivity.java
+++ Labo3/SYM_Labo3/app/src/main/java/com/example/olivier/sym_labo3/BarScanActivity.java
@@ -1,13 +1,46 @@
 package com.example.olivier.sym_labo3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class BarScanActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_scan);
+
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            // Empty QRcode
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            }
+
+            else {
+                if (result.getContents() == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
