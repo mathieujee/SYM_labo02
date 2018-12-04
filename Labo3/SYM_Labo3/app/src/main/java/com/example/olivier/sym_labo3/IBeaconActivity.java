32c615ede5d126c9d5690c3bc13f5efb31c1c9c0
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-12-04 15:28:27 +0100

    ca marche peu etre

--- Labo3/SYM_Labo3/app/src/main/java/com/example/olivier/sym_labo3/IBeaconActivity.java
+++ Labo3/SYM_Labo3/app/src/main/java/com/example/olivier/sym_labo3/IBeaconActivity.java
@@ -1,56 +1,76 @@
 package com.example.olivier.sym_labo3;
 
+import android.content.Context;
 import android.os.RemoteException;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.MonitorNotifier;
-import org.altbeacon.beacon.RangeNotifier;
-import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
-
-import java.util.Collection;
 
 public class IBeaconActivity extends AppCompatActivity implements BeaconConsumer {
-    protected static final String TAG = "MonitoringActivity";
+    protected static final String TAG = "IBeacon activity";
     private BeaconManager beaconManager;
-    private BackgroundPowerSaver backgroundPowerSaver;
+    private List<String> beaconsInfos = new ArrayList<>();
+    private ListView listView = null;
+    private ArrayAdapter<String> adapter;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ibeacon);
+        context = getApplicationContext();
+
+        adapter = new ArrayAdapter<>(this, R.layout.ibeacon_list_view, R.id.beaconsInfosTextView, beaconsInfos);
+        listView = findViewById(R.id.IBeaconListView);
+        listView.setAdapter(adapter);
+
         beaconManager = BeaconManager.getInstanceForApplication(this);
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
         beaconManager.bind(this);
-        backgroundPowerSaver = new BackgroundPowerSaver(this);
     }
+
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         beaconManager.unbind(this);
+        super.onDestroy();
     }
+
     @Override
     public void onBeaconServiceConnect() {
         beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                for (Beacon beacon : beacons) {
-                    Log.i("MainActivity", "I see a beacon that is about "+beacon.getDistance()+" meters away.");
+                beaconsInfos.clear();
+                if (beacons.size() > 0) {
+                    for (Beacon beacon : beacons) {
+                        beaconsInfos.add("RSSI : " + Integer.toString(beacon.getRssi()) + "\nMAJOR ID : " + beacon.getId2() + "\nMINOR ID : " + beacon.getId3());
+                    }
+
+                } else {
+                    beaconsInfos.add("There is no beacon in this area");
                 }
+                adapter.notifyDataSetChanged();
             }
         });
 
         try {
             beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {    }
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
+
 }
