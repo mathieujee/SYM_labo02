908ae347a3a5760bd68ca0647b9bba51e996a89f
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-12-04 09:48:27 +0100

    beacon debut

--- Labo3/SYM_Labo3/app/src/main/java/com/example/olivier/sym_labo3/IBeaconActivity.java
+++ Labo3/SYM_Labo3/app/src/main/java/com/example/olivier/sym_labo3/IBeaconActivity.java
@@ -1,13 +1,56 @@
 package com.example.olivier.sym_labo3;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class IBeaconActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+
+import java.util.Collection;
+
+public class IBeaconActivity extends AppCompatActivity implements BeaconConsumer {
+    protected static final String TAG = "MonitoringActivity";
+    private BeaconManager beaconManager;
+    private BackgroundPowerSaver backgroundPowerSaver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ibeacon);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                for (Beacon beacon : beacons) {
+                    Log.i("MainActivity", "I see a beacon that is about "+beacon.getDistance()+" meters away.");
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
     }
 }
